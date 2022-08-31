a3cecc54ecea4632f8de7e9292257f98d2da0917
JuanJJ000 <90078173+JuanJJ000@users.noreply.github.com>
2022-08-31 10:27:55 -0600

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,71 +1,136 @@
---Create database SICIF
+select count (CodProd) as CantReg,
+sum (existp) as CantP, avg (preciop) as PrecioProm, 
+min (preciop) as PrecioMax, var (preciop) as VarianzaP,
+stdev (precioP) as DevEP from Productos
 
-----a
---use SICIF
+Create view FAgregadas as
 
---Create table Proveedor(
+select  count (CodProd) as CantReg,
+sum (existp) as CantP, avg (preciop) as PrecioProm, 
+min (preciop) as PrecioMax, var (preciop) as VarianzaP,
+stdev (precioP) as DevEP from Productos
 
---Id_prov int identity (1,1) primary key not null,
---NobreProv nvarchar (40) not null,
---DirP nvarchar (50) not null
---)
+select * from FAgregadas
 
+-- Funciones Standard del Sistema
+-- 1.- Fecha/Hora 
+-- 1.1.- getdate() : Retorna la fecha y la hora del sistema
+select GETDATE () as FechaHoraActual
 
---insert into Proveedor values ('GRUMA','Carretera a Masaya'),
---('FEMSA' ,'Carretera Norte')
+--1.2.- Year(): Retornar el año de una fech
+select year(getdate()) as AñoActual,
+year('19710619') as MiAñoNac
 
---select * from Proveedor
-
-
---Usuario/Admin
-
---sp_addlogin 'Juan','GomezArauz2022','SICIF'
---sp_addsrvrolemember 'Juan','sysadmin'
-
---sp_adduser 'Juan','Phd'
-
---sp_addrolemember 'db_ddladmin','Phd'
-
-
--- Usuario Solo lectura
-
---sp_addlogin 'Estudiantes','ESistemas2022','SICIF'
---sp_addsrvrolemember 'Estudiantes','processadmin'
---use SICIF	
---sp_adduser 'Estudiantes','Estudiantes'
-
---sp_addrolemember 'db_denydatawriter','Estudiantes'
---sp_addrolemember 'db_datareader','Estudiantes'
-
---revoke create table, Create view, create procedure to Estudiantes
-
---deny insert, delete, update on Proveedor to Estudiantes
-
---Usuario solo escritura
+--1.3.- Month(): Retorna el mes de una fecha select month()
+select month(getdate()) as MesActual,
+month('19710619') as MiMesNac
 
 
---sp_addlogin 'Cajero','Cajero2022','SICIF'
---sp_addsrvrolemember 'Cajero','processadmin'
---use SICIF	
---sp_adduser 'Cajero','Cajero'
+--1.4.- Month(): Retorna el mes de una fecha select month()
+select day(getdate()) as DiaActual,
+day ('19710619') as MidiaNac
+  
 
---sp_addrolemember 'db_denydatareader','Cajero'
+--1.5.- datediff(): Diferenciar fechas
+select DATEDIFF(YEAR,'19710619',getdate()) as
+edadA, DATEDIFF(month,'19710619',getdate()) as
+edadM, DATEDIFF(week,'19710619',getdate()) as
+edadS, DATEDIFF(day,'19710619',getdate()) as
+edadD, DATEDIFF(hour,'19710619',getdate()) as EdadH
 
---sp_addrolemember 'db_datawriter','Cajero'
+--1.6.- dateadd(): Sumar fechas
+
+select dateadd(YEAR,'19710619',getdate()) as
+edadA, dateadd(month,'19710619',getdate()) as
+edadM, dateadd(week,'19710619',getdate()) as
+edadS, dateadd(day,'19710619',getdate()) as
+edadD, dateadd(hour,'19710619',getdate()) as EdadH
+
+--1.7.- datename()
+SELECT DATENAME(year, getdate()) as AñoActual 
+    ,DATENAME(month, getdate()) as mesactual 
+    ,DATENAME(day, getdate()) as diactual 
+    ,DATENAME(dayofyear, getdate()) as diadelaño 
+    ,DATENAME(weekday, getdate()) as diasem
+
+--1.8.- datepart()
+
+select datepart(year,getdate()) as AñoActual
+
+--2.- Cadena
+--2.1-- len(): devuelve la longitud de una cadena
+
+select len(NombreProd) as LNP from Productos
+
+--2.2.- Char(): Devuelve el caracter a partir de su codigo ascii
+select char(61) as Caract
+
+--2.3.- Ascii(): Devuelve el codigo asccii de un caracter
+select ascii ('ñ') as ValorA
+
+--2.4.- Lower(): Tramsforma una cadena a minuscula
+select lower ('ASH KETCHUM') as Minun
+
+--2.5.- Upper(): Tramsforma una cadena a Mayuscuña
+select upper ('ASH KETCHUM') as Plusle
+
+--2.6.- Substringt(): Abstraer una subcadena de una cadena
+select substring ('001-190671-0011Y',5,6) as FechaNac
+
+--2.7.- Charindex(): Coincidencia a partir de un indice
+DECLARE @document VARCHAR(64);  
+SELECT @document = 'Reflectors are vital safety' +  
+                   ' components of your bicycle.';  
+SELECT CHARINDEX('bicycle', @document) as indice
+
+--2.8.- Concat(): Concatenar cadenas
+SELECT CONCAT ( 'Happy ', 'Birthday ', 11, '/', '25' ) AS Result
+
+--2.9.- reverse(): revierte una cadena
+SELECT reverse ( 'Angel' ) AS NombreInvertido;
+
+--2.10.- Ledt() valores a partir de una posicion izquierda
+select left ('Msc. en computacion',8) as ParteIzq
+
+--2.11.- Right() valores a partir de una posicion derecha
+select right ('Msc. en computacion',8) as ParteDer
+
+--2.12.- LTrim() Elimia espacios blanco a la iz1
+select LTrim ('Msc. en computacion') as EBI
+
+--2.13.- LTrim() Elimia espacios blanco a la iz1
+select RTrim ('Msc. en computacion') as EBD
+
+--3 .- Convertir
+
+--Cast, convert, Parse, try_cast , ry_convert, try_parse
+
+--3.1.- Convert
+declare @ani as int
+set @ani=(select cast(SUBSTRING('001-190671-0011Y',9,2) as int ) 
+as AñoNac) 
+print @ani
+
+--3.2 .Convert
+declare @anint as int
+set @anint=(select convert(int, SUBSTRING('001-190671-0011Y',9,2)) 
+as AñoNac) 
+print @anint
+
+--3.3.- Parse
+
+Select Parse('Monday, 13 december 2010' as datetime2 using 'en-US') AS Result
+
+--3.4.- Calcular edad
 
 
---revoke create table, Create view, create procedure to Cajero
-             
---deny insert, delete, update on Proveedor to Cajero
+
+DECLARE @Carvallo datetime  
+set @Carvallo =(select Parse('Monday, 13 december 2010' as datetime2 using 'en-US') as datetime)
+select day (@Carvallo ) as DIADENACIMIENTO
 
 
---Usuario L/E
-
---sp_addlogin 'Docente','Cajero2022','SICIF'
---sp_addsrvrolemember 'Cajero','processadmin'
---use SICIF	
---sp_adduser 'Cajero','Cajero'
-
---sp_addrolemember 'db_denydatareader','Cajero'
-
---sp_addrolemember 'db_datawriter','Cajero'
+declare @anint as int
+set @anint=(select convert(int, SUBSTRING('001-190671-0011Y',9,2)) 
+as AñoNac) 
+print @anint
